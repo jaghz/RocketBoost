c9e7f6372bdf1a9d31163d8464b0ea5d69a34ffd
jaghz <jill.a.zalat@gmail.com>
2021-03-20 10:25:28 -0400

    Input binding

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("space pressed!");
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("left pressed!");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("right pressed!");
+        }
+    }
+        
+
+    
+}
